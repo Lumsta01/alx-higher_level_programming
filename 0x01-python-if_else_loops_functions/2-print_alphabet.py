5f32870726d05e0b8df16645036a3da6ee567978
Lumsta01 <lulamamanganyi01@gmail.com>
2023-03-07 18:43:27 -0800

    update task

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-ascii_alphabet = range(97, 123)
-
-for letter in ascii_alphabet:
-    print(chr(letter))
+for alphabet in range(97, 123):
+    print("{}".format(chr(alphabet)), end="")
