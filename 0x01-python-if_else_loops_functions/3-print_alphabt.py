2a567740c82c35dc3fe0823a51c26aef412bc0a6
Lumsta01 <lulamamanganyi01@gmail.com>
2023-03-07 18:51:40 -0800

    update task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for alphabet in range(97, 123):
     if alphabet == 101 or alphabet == 113:
-        break
+        continue
     else:
         print("{}".format(chr(alphabet)), end="")
