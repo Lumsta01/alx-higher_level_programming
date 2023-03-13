157ea7f9d7ea80070bf2b90024771984515afc6c
Lumsta01 <lulamamanganyi01@gmail.com>
2023-03-12 19:00:47 -0700

    fix format error

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
-
-    for i in my_list:
-        print(i)
+    for list_element in range(len(my_list)):
+        print("{:d}".format(my_list[list_element]))
