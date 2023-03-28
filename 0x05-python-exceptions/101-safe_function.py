37c394ce86d48d6cdec131c89fc17931f27a4531
Lumsta01 <lulamamanganyi01@gmail.com>
2023-03-27 18:08:10 -0700

    Checker Update

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+from sys import stderr
 
-import sys
 
 def safe_function(fct, *args):
     try:
         result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    except BaseException as e:
+        print("Exception: {}".format(e), file=stderr)
+        result = None
+    finally:
+        return result
