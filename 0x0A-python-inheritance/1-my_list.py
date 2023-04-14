21a6481e38cc75c293559ff908138513b8294463
Lumsta01 <lulamamanganyi01@gmail.com>
2023-04-13 19:51:03 -0700

    task update

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-
-"""
-class MyList that inherits a list."""
+"""Define empty class BaseGeometry."""
 
 
-class MyList(list):
-    """A custom list class that adds a method for printing the sorted list"""
-
-    def print_sorted(self):
-        """Print the list, sorted in ascending order"""
-        print(sorted(self))
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
